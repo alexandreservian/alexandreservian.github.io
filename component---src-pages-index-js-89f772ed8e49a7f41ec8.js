4fa6c6a7d0122766edd3a7329e2354e5d60fb071
alexandreservian <alexandreservian@gmail.com>
2020-05-24 18:07:44 -0300

    Updates

---
+++ component---src-pages-index-js-89f772ed8e49a7f41ec8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RXBc:function(e,n,t){"use strict";t.r(n),t.d(n,"pageQuery",(function(){return c}));var a=t("q1tI"),r=t.n(a),l=t("I/Ru"),u=function(e){var n=e.posts;return console.log(n),r.a.createElement("ul",null,r.a.createElement("li",null))},o=t("vrFN"),c=(n.default=function(e){var n=e.data.allMarkdownRemark.edges;return r.a.createElement(l.a,null,r.a.createElement(o.a,{title:"Home"}),r.a.createElement(u,{posts:n}))},"1092673306")}}]);
+//# sourceMappingURL=component---src-pages-index-js-89f772ed8e49a7f41ec8.js.map
