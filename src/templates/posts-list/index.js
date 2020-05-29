75c3d597c5e7a9f31ecb0cc43fab4776d182ab8b
alexandreservian <alexandreservian@gmail.com>
2020-05-29 20:29:52 -0300

    refactor: listing posts

--- src/templates/posts-list/index.js
+++ src/templates/posts-list/index.js
@@ -1,11 +1,17 @@
 import React from "react"
+import PostLink from "../post-link"
+import { Content, Item } from "./style"
 
 const PostsList = ({ posts }) => {
   console.log(posts)
   return (
-    <ul>
-      <li></li>
-    </ul>
+    <Content>
+      {posts.map(({ node: { id, frontmatter } }) => (
+        <Item key={id}>
+          <PostLink {...frontmatter} />
+        </Item>
+      ))}
+    </Content>
   )
 }
 
---
+++ src/templates/posts-list/style.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled"
+
+export const Content = styled.ul`
+  margin: 50px auto 0;
+  list-style: none;
+  width: 980px;
+`
+
+export const Item = styled.li`
+  margin 0;
+  padding: 0 20px;
+`
